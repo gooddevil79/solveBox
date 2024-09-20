6e91e0b431b648c7c139617acb81469ec4a6b981
Mehran <behroziyan.mhrn@gmail.com>
2024-09-20 11:18:41 +0330

    refactor(codeforces:A): remove redundent fn in beautiful matrix

--- codeforces/_A/Beautiful Matrix.js
+++ codeforces/_A/Beautiful Matrix.js
@@ -14,34 +14,9 @@ rl.on("line", line => {
 
 const countStepsToCenter = (row, col) =>
 	Math.abs(row - GOAL_INDEX) + Math.abs(col - GOAL_INDEX);
-const solvedMatrix = (matrix, goalPlace, { targetCol, targetRow }) => {
-	const result = [...matrix];
-	if (targetRow > goalPlace) {
-		result = result.forEach((row, index) => {
-			[result[index], result[index - 1]] = [result[index - 1], row];
-		});
-	} else {
-		result = result.forEach((row, index) => {
-			[result[index], result[index - 1]] = [result[index - 1], row];
-		});
-	}
-	if (targetCol > goalPlace) {
-		result = result.forEach((row, index) => {
-			[result[index], result[index - 1]] = [result[index - 1], row];
-		});
-	} else {
-		result = result.forEach((row, index) => {
-			[result[index], result[index - 1]] = [result[index - 1], row];
-		});
-	}
-};
 
 rl.on("close", () => {
 	const [targetRow, targetCol] = findTarget(matrix);
-	// const solvedMatrix = solveMatrix(matrix, GOAL_INDEX, {
-	// 	targetRow,
-	// 	targetCol,
-	// });
 	const steps = countStepsToCenter(targetRow, targetCol);
 	console.log(steps);
 });
